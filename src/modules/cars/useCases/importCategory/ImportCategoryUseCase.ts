6bddbb54cc6258de4ad592136aa5cc98ff3cba72
Rafael Cena <rafael.cena@hotmail.com>
2022-07-29 15:56:29 -0300

    trabalhando o upload e salvando

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,19 +1,50 @@
 import fs from 'fs'
 import {parse} from 'csv-parse'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase {
 
-    execute(file:Express.Multer.File):void{
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    loadCategories(file:Express.Multer.File):Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+
+            const stream = fs.createReadStream(file.path);
+            
+            const categories: IImportCategory[] = [];
+
+            const parseFile = parse()
+
+            stream.pipe(parseFile)
+
+            parseFile.on('data', async (line) => {
+                const [name, description] = line;
+
+                categories.push({ name,description });
+            })
+            .on('end', () =>{
+                resolve(categories)
+            })
+            .on('error', (err) =>{
+                reject(new Error('Error while parsing file ' + err))
+            })
+
+           
+        }
+        )
+    }
+
+    async execute(file:Express.Multer.File): Promise<void> {
         
-        const stream = fs.createReadStream(file.path);
+        const categories = await this.loadCategories(file);
 
-        const parseFile = parse()
-
-        stream.pipe(parseFile)
-
-        parseFile.on('data', (data) => {
-            console.log(data)
-        })
+        console.log(categories);
 
     }
 }
