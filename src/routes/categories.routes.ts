4bb8e4a22c1ef3136b3f86b67418730ca061c6fb
Rafael Cena <rafael.cena@hotmail.com>
2022-07-29 14:35:17 -0300

    feat(import category)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
-import {Router} from 'express';
+import {response, Router} from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
+import multer from 'multer';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: './tmp' });
 
 categoriesRoutes.post('/', (req, res) => {
      return createCategoryController.handle(req, res);
@@ -11,6 +15,12 @@ categoriesRoutes.post('/', (req, res) => {
 
 categoriesRoutes.get('/', (req, res) => {
       return listCategoryController.handle(req, res);
+})
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+
+    return importCategoryController.handle(req, res);
+
 })
 
 export {categoriesRoutes}
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ app.use(express.json());
 
 app.use(router)
 
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
