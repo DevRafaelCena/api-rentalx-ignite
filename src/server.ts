c327f47f1eb890d2ae87096a2cec1b0680af532e
Rafael Cena <rafael.cena@hotmail.com>
2022-08-03 08:00:45 -0300

    docker compose

--- src/server.ts
+++ src/server.ts
@@ -12,5 +12,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router)
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000 teste');
 })
